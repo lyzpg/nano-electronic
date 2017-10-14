60932a0b4e4a287f428de7f17b4434de7103a5ab
Ivan Elv <Ivan Elv>
2017-10-14 23:52:13 +0700

    Header Created.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
+/* Fungsi Untuk Load Header*/
 function loadHeader()
 {
-	
+	fetch(location.href.replace("index.html", "") + "master_header.html")
+		.then(response => response.text())
+		.then(data => document.querySelectorAll(".header")[0].innerHTML = data);
 }
