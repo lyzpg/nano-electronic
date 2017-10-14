d48d6251dbe78b076e4432d30a162c45a3ecddb0
Ivan Elv <Ivan Elv>
2017-10-15 00:30:44 +0700

    Navbar created.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 /* Fungsi Untuk Load Header*/
+
+function loadComponent()
+{
+	loadHeader();
+	loadNavbar();
+}
+
 function loadHeader()
 {
 	fetch(location.href.replace("index.html", "") + "master_header.html")
 		.then(response => response.text())
 		.then(data => document.querySelectorAll(".header")[0].innerHTML = data);
+}
+
+function loadNavbar()
+{
+	fetch(location.href.replace("index.html", "") + "master_nav.html")
+		.then(response => response.text())
+		.then(data => document.querySelectorAll(".nav")[0].innerHTML = data);
 }
