b3d02745ef2dc519be6770b44d2bbcdbfb00a8f5
Ivan Elv <Ivan Elv>
2017-10-16 13:31:47 +0700

    Footer and content created.

--- js/index.js
+++ js/index.js
@@ -4,18 +4,45 @@ function loadComponent()
 {
 	loadHeader();
 	loadNavbar();
+	loadFooter();
 }
 
 function loadHeader()
 {
-	fetch(location.href.replace("index.html", "") + "master_header.html")
+	fetch("master_header.html")
 		.then(response => response.text())
 		.then(data => document.querySelectorAll(".header")[0].innerHTML = data);
 }
 
 function loadNavbar()
 {
-	fetch(location.href.replace("index.html", "") + "master_nav.html")
+	/**
+	 * Load The Navbar
+  	 */
+	var navbar = fetch("master_nav.html")
 		.then(response => response.text())
 		.then(data => document.querySelectorAll(".nav")[0].innerHTML = data);
+
+	/**
+	 * Set Active Tab
+	 */
+	Promise.resolve(navbar)
+		.then(() => activePage());
+}
+
+function loadFooter()
+{
+	fetch("master_footer.html")
+		.then(response => response.text())
+		.then(data => document.querySelectorAll(".footer")[0].innerHTML = data);
+}
+
+function activePage()
+{
+	var page = document.querySelectorAll("meta[name=active-page]")[0].getAttribute("content");
+	try
+	{
+		document.getElementById(page).classList.add("active");
+	}
+	catch (exception) { }
 }
