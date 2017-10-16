712c5c31436c6da69656a890f4e1c17a54f82df1
Ivan Favian Elianto <purenaturalism@gmail.com>
2017-10-16 15:11:52 +0700

    Login page and system flow created.

--- js/index.js
+++ js/index.js
@@ -9,32 +9,56 @@ function loadComponent()
 
 function loadHeader()
 {
-	fetch("master_header.html")
-		.then(response => response.text())
-		.then(data => document.querySelectorAll(".header")[0].innerHTML = data);
+	/**
+	 * Surrond with try catch because it have chance of Networking problem.
+	 */
+	try
+	{
+		fetch("master_header.html")
+			.then(response => response.text())
+			.then(data => document.querySelectorAll(".header")[0].innerHTML = data);
+	}
+	catch (exception)
+	{ }
 }
 
 function loadNavbar()
 {
 	/**
-	 * Load The Navbar
-  	 */
-	var navbar = fetch("master_nav.html")
-		.then(response => response.text())
-		.then(data => document.querySelectorAll(".nav")[0].innerHTML = data);
-
-	/**
-	 * Set Active Tab
+	 * Surrond with try catch because it have chance of Networking problem.
 	 */
-	Promise.resolve(navbar)
-		.then(() => activePage());
+  	try
+  	{
+		/**
+		 * Load The Navbar
+	  	 */
+		var navbar = fetch("master_nav.html")
+			.then(response => response.text())
+			.then(data => document.querySelectorAll(".nav")[0].innerHTML = data);
+
+		/**
+		 * Set Active Tab
+		 */
+		Promise.resolve(navbar)
+			.then(() => activePage());
+	}
+	catch (exception)
+	{ }
 }
 
 function loadFooter()
 {
-	fetch("master_footer.html")
-		.then(response => response.text())
-		.then(data => document.querySelectorAll(".footer")[0].innerHTML = data);
+	/**
+	 * Surrond with try catch because it have chance of Networking problem.
+	 */
+	try
+	{
+		fetch("master_footer.html")
+				.then(response => response.text())
+				.then(data => document.querySelectorAll(".footer")[0].innerHTML = data);
+	}
+	catch (exception)
+	{ }
 }
 
 function activePage()
@@ -45,4 +69,38 @@ function activePage()
 		document.getElementById(page).classList.add("active");
 	}
 	catch (exception) { }
+}
+
+function validateLoginInput()
+{
+	var error = false;
+
+	var inputs = document.querySelectorAll("input");
+
+	for (var i = 0; i < inputs.length; i++)
+	{
+		var element = document.querySelectorAll("h4")[i];
+		var elementName = inputs[i].getAttribute("for");
+
+		if (inputs[i].value == "")
+		{
+			element.innerHTML = elementName + " can't be empty!";
+			element.style.color = "#A94442";
+			error = true;
+		}
+		else
+		{
+			element.innerHTML = elementName;
+			element.style.color = "#8B8787";
+		}
+	}
+	return error;
+}
+
+function login()
+{
+	if (!validateLoginInput())
+	{
+		alert("Login Berhasil!");
+	}
 }
